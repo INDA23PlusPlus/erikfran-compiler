9b2b8390684cb7de09d43aecde418bb5e8272682
ErikFrankling <erik.frankling@frankling.se>
2023-11-09 13:01:34 +0100

    more emulator things

--- src/emulator_compiler.rs
+++ src/emulator_compiler.rs
@@ -1,11 +1,142 @@
+use std::{hash::Hash, collections::HashMap};
+
 use crate::ast::*;
 
+enum Allocation {
+    Register(u8),
+    Memory(u8),
+}
+
+enum Lifetime {
+    Alloc(String),
+    Free(String),
+}
+
+struct State {
+    available_registers: [bool; 16],
+    available_memory: [bool; 256],
+    variable_map: HashMap<String, Allocation>,
+    lifetime_vec: Vec<Lifetime>,
+}
+
 pub fn compile(ast: Ast) -> String {
+    let mut state = State {
+        available_registers: [true; 16],
+        available_memory: [true; 256],
+        variable_map: HashMap::new(),
+        lifetime_vec: Vec::new(),
+    };
+
+    block_lifetime(&*ast.ast, &mut state);
+
     let mut assembly_code = String::new();
 
     assembly_code.push_str(&block_comp(&*ast.ast));
 
     assembly_code
+}
+
+fn allocate(variable: &str, state: &mut State) {
+    for (i, register) in state.available_registers.iter_mut().enumerate() {
+        if *register {
+            state.available_registers[i] = false;
+            state.variable_map.insert(variable.to_string(), Allocation::Register(i as u8));
+            return;
+        }
+    }
+
+    for (i, memory) in state.available_memory.iter_mut().enumerate() {
+        if *memory {
+            state.available_memory[i] = false;
+            state.variable_map.insert(variable.to_string(), Allocation::Memory(i as u8));
+            return;
+        }
+    }
+
+    panic!("Out of memory!");
+}
+
+fn register_allocate(variable: &str, state: &mut State) {
+    if let Some(allocation) = state.variable_map.get(variable) {
+        match allocation {
+            Allocation::Register(_) => {},
+            Allocation::Memory(address) => {
+                for (i, register) in state.available_registers.iter_mut().enumerate() {
+                    if *register {
+                        state.available_registers[i] = false;
+                        state.available_memory[*address as usize] = true;
+
+                        state.variable_map.remove(variable);
+                        state.variable_map.insert(variable.to_string(), Allocation::Register(i as u8));
+                        return;
+                    }
+                }
+
+                
+            },
+        }
+    }
+    else {
+        panic!("Variable {} not found!", variable);
+    }
+}
+
+fn block_lifetime(block: &Vec<Statement>, state: &mut State) {
+    for statement in block {
+        match statement {
+            Statement::Print { expr } => {
+                expr_lifetime(expr, state);
+            },
+            Statement::Let { variable, expr } => {
+                state.lifetime_vec.push(Lifetime::Alloc(variable.clone()));
+                allocate(variable.as_str(), state);
+                expr_lifetime(expr, state);
+            },
+
+        }
+    }
+}
+
+fn expr_lifetime(expr: &Expression, state: &mut State) {
+    match expr {
+        Expression::Term(term) => term_lifetime(term, state),
+        Expression::Plus { expr, term } => {
+            expr_lifetime(expr, state);
+            term_lifetime(term, state);
+        },
+        Expression::Minus { expr, term } => {
+            expr_lifetime(expr, state);
+            term_lifetime(term, state);
+        },
+    }
+}
+
+fn term_lifetime(term: &Term, state: &mut State) {
+    match term {
+        Term::Factor(factor) => factor_lifetime(factor, state),
+        Term::Divide { term, factor } => {
+            term_lifetime(term, state);
+            factor_lifetime(factor, state);
+        },
+        Term::Multiply { term, factor } => {
+            term_lifetime(term, state);
+            factor_lifetime(factor, state);
+        },
+    }
+}
+
+fn factor_lifetime(factor: &Factor, state: &mut State) {
+    match factor {
+        Factor::Int(_) => {},
+        Factor::Variable(name) => {
+            if !state.variable_map.contains_key(name) {
+                panic!("Variable {} not found!", name);
+            }
+        },
+        Factor::Parenthesis(expr) => {
+            expr_lifetime(expr, state);
+        },
+    }
 }
 
 fn block_comp(block: &Vec<Statement>) -> String {
@@ -14,7 +145,7 @@ fn block_comp(block: &Vec<Statement>) -> String {
     for statement in block {
         out.push_str(&match statement {
             Statement::Print { expr } => {
-                format!("{}setmr\n", expr_comp(expr))
+                format!("{}setmr {:02X} 0xFF\n", expr_comp(expr), )
             },
             Statement::Let { variable, expr } => {
                 format!("    let mut {} = {};\n", variable, expr_comp(expr))
